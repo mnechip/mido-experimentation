e4f2aa284e95f01b1d78136fb7bafee8a4dcd37d
Maxim Nechiporenko <Maxim.Nechiporenko1@education.nsw.gov.au>
2021-02-08 15:43:01 +1100

    Outputs built in midi file to midiOutput.txt through append_new_line module. Uses mido and numpy library.

---
+++ mido_form/mido_form_v01/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        obj = CreateObject("Wscript.Shell")
+        obj.run("C:\Users\abc\source\repos\mnechip\mido-experimentation")
+    End Sub
+End Class
---
+++ mido_form/mido_form_v01/mido_form_01.vb
@@ -0,0 +1,6 @@
+﻿Public Class mido_form_01
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ReturnValue
+        ReturnValue = Process.Start("C:\python39\python ""C:\Users\abc\source\repos\mnechip\mido-experimentation.py"" ", vbHide)
+    End Sub
+End Class
